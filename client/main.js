60bb6007cc6a346aa4f6c0ce06271abb00833a9e
Daniel <danielabdi12@gmail.com>
2023-01-10 16:45:48 +0100

    pre fly

--- client/main.js
+++ client/main.js
@@ -18,5 +18,5 @@ socket.on('chat message', function(msg) {
   var item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  window.scrollTo(0, document.body.scrollHeight); 
 });
--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:53141", "http://localhost:3001", "http://127.0.0.1:5173"]
+    origin: ["http://localhost:53141", "http://localhost:3001", "http://127.0.0.1:5173", "http://127.0.0.1:3005"]
   }
 });
 
 io.on('connection', (socket) => {
-  console.log(socket.id, 'a user connected');
+  console.log(socket.id, 'a user connected!!!');
   socket.on('disconnect', () => {
     console.log(socket.id, 'user disconnected');
   });
@@ -24,7 +24,6 @@ io.on('connection', (socket) => {
     io.emit("place-event", event)
   })
 });
-
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
